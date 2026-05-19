c96d21dcfcfaabe00f010bbed6a69f7b52022b0c
Carson Bantz <bantzcarson@gmail.com>
2026-05-19 09:40:24 -0500

    Added Quantum Efficiency function in its own script for use across scripts.

---
+++ Quantum_Efficiency.py
@@ -0,0 +1,30 @@
+from CXRO_Tools import Filter_Transmission
+from scipy import interpolate
+import numpy as np
+import matplotlib.pyplot as plt
+
+def QE_Interpolated_Function(min : int, max : int, Si_thickness : float, C_thickness : float, SiO2_thickness : float, Fe_thickness : float, step : int = 100):
+    cxro = Filter_Transmission()
+    Si_cxro_data = cxro.get_transmission("Si", thickness = Si_thickness, energy_min=min, energy_max=max, steps=step)
+    C_cxro_data = cxro.get_transmission("C", thickness= C_thickness, energy_min=min, energy_max=max, steps=step)
+    SiO2_cxro_data = cxro.get_transmission("SiO2", thickness= SiO2_thickness, energy_min=min, energy_max=max, steps=step)
+    Fe_cxro_data = cxro.get_transmission("Fe", thickness= Fe_thickness, energy_min=min, energy_max=max, steps=step)
+
+    energies = np.array([i[0] for i in Si_cxro_data])
+    Si_transmission = np.array([i[1] for i in Si_cxro_data])
+    C_transmission = np.array([i[1] for i in C_cxro_data])
+    SiO2_transmission = np.array([i[1] for i in SiO2_cxro_data])
+    Fe_transmission = np.array([i[1] for i in Fe_cxro_data])
+
+    QE = (1-Si_transmission) * C_transmission * SiO2_transmission * Fe_transmission
+    interpolated_QE = interpolate.PchipInterpolator(energies, QE)
+
+
+    return interpolated_QE
+
+if __name__ == "__main__":
+    func = QE_Interpolated_Function(30, 2000, 5, 0.1, 0.1, 0.05, 100)
+    test_range = range(30, 2000, 50)
+    plt.plot(test_range, func(test_range))
+    plt.show()
+    
