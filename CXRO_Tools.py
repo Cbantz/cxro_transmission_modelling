042d70f0647025721701a9ff8f4dcd5b50da212d
Cbantz <bantzcarson@gmail.com>
2026-06-16 14:59:27 -0500

    Fixed typos, allowed for not using database.
    Can use the 'use_database' argument. If false, will pull straight from CXRO for each try. This is useful for leastsq fitting, though much slower.
    Fixed energy_min vs min (same for max) typos

--- CXRO_Tools.py
+++ CXRO_Tools.py
@@ -3,82 +3,82 @@ import bs4
 import urllib
 import numpy as np
 
-class Filter_Transmission:
-    """
-    Connects to CXRO to work with their Filter Transmission calculator
+# class Filter_Transmission:
+#     """
+#     Connects to CXRO to work with their Filter Transmission calculator
 
-    Key Methods:
-        get_transmission: Returns a list of ordered pairs corresponding to energy and transmission for a given material and thickness.
-    """
-    URLBASE = 'https://henke.lbl.gov'
-    MAX_STEP = 999
-    script = "filter.pl"
-    def __init__(self):
-        pass
+#     Key Methods:
+#         get_transmission: Returns a list of ordered pairs corresponding to energy and transmission for a given material and thickness.
+#     """
+#     URLBASE = 'https://henke.lbl.gov'
+#     MAX_STEP = 999
+#     script = "filter.pl"
+#     def __init__(self):
+#         pass
 
-    def _post(self, data):
-        url = urllib.request.urljoin(self.URLBASE, '/cgi-bin/' + self.script)
-        return requests.post(url, data)
+#     def _post(self, data):
+#         url = urllib.request.urljoin(self.URLBASE, '/cgi-bin/' + self.script)
+#         return requests.post(url, data)
     
-    def _retrieve_data(self, response):
-        soup = bs4.BeautifulSoup(response.text, 'html.parser')
-        urldata = soup.body.h2.find('a').attrs['href']
+#     def _retrieve_data(self, response):
+#         soup = bs4.BeautifulSoup(response.text, 'html.parser')
+#         urldata = soup.body.h2.find('a').attrs['href']
 
-        url = urllib.request.urljoin(self.URLBASE, urldata)
-        with urllib.request.urlopen(url) as fp:
-            return fp.read()
+#         url = urllib.request.urljoin(self.URLBASE, urldata)
+#         with urllib.request.urlopen(url) as fp:
+#             return fp.read()
 
-    def _parse_data(self, s):
-        return [list(map(float, line.split())) for line in s.splitlines()[2:]]
+#     def _parse_data(self, s):
+#         return [list(map(float, line.split())) for line in s.splitlines()[2:]]
     
-    def _process(self, data):
-        response = self._post(data)
-        s = self._retrieve_data(response)
-        return self._parse_data(s)
+#     def _process(self, data):
+#         response = self._post(data)
+#         s = self._retrieve_data(response)
+#         return self._parse_data(s)
     
-    def _create_data(self, chemical_formula : str, thickness : float, energy_min : int, energy_max : int, steps : int = 100):
-        """Returns dictionary containing all necessary values for accessing CXRO Filter Transmission Data"""
-        data = {}
-        data['Materia'] = "Enter Formula"
-        data['Formula'] = chemical_formula
-        data['Density'] = -1
-        data['Thickness'] = thickness
-        data['Min'] = energy_min
-        data['Max'] = energy_max
-        data['Npts'] = steps
-        data['Plot'] = "Linear"
-        data['Output'] = "Plot"
-        return data
+#     def _create_data(self, chemical_formula : str, thickness : float, energy_min : int, energy_max : int, steps : int = 100):
+#         """Returns dictionary containing all necessary values for accessing CXRO Filter Transmission Data"""
+#         data = {}
+#         data['Materia'] = "Enter Formula"
+#         data['Formula'] = chemical_formula
+#         data['Density'] = -1
+#         data['Thickness'] = thickness
+#         data['Min'] = energy_min
+#         data['Max'] = energy_max
+#         data['Npts'] = steps
+#         data['Plot'] = "Linear"
+#         data['Output'] = "Plot"
+#         return data
 
 
-    def get_transmission(self, chemical_formula : str, thickness : float, energy_min : int, energy_max : int, steps : int = 100):
-        """
-        Returns ordered pairs of [energy (eV), transmission]. Takes arguments for options on CXRO Filter Transmission calculator.
+#     def get_transmission(self, chemical_formula : str, thickness : float, energy_min : int, energy_max : int, steps : int = 100):
+#         """
+#         Returns ordered pairs of [energy (eV), transmission]. Takes arguments for options on CXRO Filter Transmission calculator.
 
-        Args:
-            chemical_formula (str): The chemical formula of your filter. E.g. Si, Si3N4
-            thickness (float): Thickness (in microns) of your filter.
-            energy_min (int): Minimum energy to be included in dataset.
-            energy_max (int): Maximum energy to be included in dataset.
-            steps (int, optional): Number of steps between min and max energies to be included in dataset. Defaults to 100.
+#         Args:
+#             chemical_formula (str): The chemical formula of your filter. E.g. Si, Si3N4
+#             thickness (float): Thickness (in microns) of your filter.
+#             energy_min (int): Minimum energy to be included in dataset.
+#             energy_max (int): Maximum energy to be included in dataset.
+#             steps (int, optional): Number of steps between min and max energies to be included in dataset. Defaults to 100.
 
-        Returns:
-            list: List of ordered pairs relating each energy in dataset to its transmission.
-        """
-        data = self._create_data(chemical_formula, thickness, energy_min, energy_max, steps)
+#         Returns:
+#             list: List of ordered pairs relating each energy in dataset to its transmission.
+#         """
+#         data = self._create_data(chemical_formula, thickness, energy_min, energy_max, steps)
 
-        return self._process(data)
+#         return self._process(data)
     
 
 URLBASE = 'https://henke.lbl.gov'
 MAX_STEP = 999
 script = "filter.pl"
 
-def _post(self, data):
+def _post(data):
     url = urllib.request.urljoin(URLBASE, '/cgi-bin/' + script)
     return requests.post(url, data)
 
-def _retrieve_data(self, response):
+def _retrieve_data(response):
     soup = bs4.BeautifulSoup(response.text, 'html.parser')
     urldata = soup.body.h2.find('a').attrs['href']
 
--- Quantum_Efficiency_Local.py
+++ Quantum_Efficiency_Local.py
@@ -1,32 +1,38 @@
 from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
+from collections.abc import Sequence
 from database_tools import add_database_item, get_formatted_filepath
+from Element_Layer import Element_Layer
+from CXRO_Tools import get_transmission
 
-def QE_Interpolated_Function(x, min : float, max : float, composition : list[dict], step : float = 100):
+def QE_Interpolated_Function(x, energy_min : float, energy_max : float, composition : Sequence[Element_Layer], steps : float = 100, use_database : bool = True):
     #Conversion from float to int to work between lmfit and cxro. lmfit sends floats.
-    min=int(min)
-    max=int(max)
-    step=int(step)
+    energy_min=int(energy_min)
+    energy_max=int(energy_max)
+    steps=int(steps)
 
 
     detector_transmission_arrays = []
     other_transmission_arrays = []
 
-    for i in composition:
-        add_database_item(chemical_formula=i["Chemical Formula"], thickness=i["Thickness"], energy_min=min, energy_max=max, steps=step)
-        i["cxro_data"] = np.load(get_formatted_filepath(chemical_formula=i["Chemical Formula"], thickness=i["Thickness"], energy_min=min, energy_max=max, steps=step))
-        if i["is_detector"] == True:
-            detector_transmission_arrays.append(i["cxro_data"][:,1])
+    for element in composition:
+        if(use_database):
+            add_database_item(chemical_formula=element.chemical_formula, thickness=element.thickness, energy_min=energy_min, energy_max=energy_max, steps=steps)
+            element.cxro_data = np.load(get_formatted_filepath(chemical_formula=element.chemical_formula, thickness=element.thickness, energy_min=energy_min, energy_max=energy_max, steps=steps))
         else:
-            other_transmission_arrays.append(i["cxro_data"][:,1])
+            element.cxro_data = np.array(get_transmission(chemical_formula=element.chemical_formula, thickness=element.thickness, energy_max=energy_max, energy_min=energy_min, steps=steps))
+        if element.is_detector == True:
+            detector_transmission_arrays.append(element.cxro_data[:,1])
+        else:
+            other_transmission_arrays.append(element.cxro_data[:,1])
 
 
 
 
 
 
-    energies = composition[0]["cxro_data"][:,0]
+    energies = composition[0].cxro_data[:,0]
 
 
     detector_transmission = np.prod(detector_transmission_arrays, 0)
@@ -37,21 +43,25 @@ def QE_Interpolated_Function(x, min : float, max : float, composition : list[dic
     return interpolated_QE(x)
 
 if __name__ == "__main__":
+    from time import process_time
     min = 30
     max = 2000
     x = np.arange(80, 1200, 50)
     composition = [
-        {"Chemical Formula" : "Si",
-         "Thickness" : 1,
-         "is_detector": True
-        },
-        {
-         "Chemical Formula" : "SiO2",
-         "Thickness" : 0.1,
-         "is_detector": False
-        }
+        Element_Layer(
+            chemical_formula="Si",
+            thickness=1,
+            is_detector=True
+        ),
+        Element_Layer(
+            chemical_formula="SiO2",
+            thickness=0.1
+        )
     ]
+    start_time = process_time()
     transmission = QE_Interpolated_Function(x, min, max, composition)
+    end_time = process_time()
+    print(end_time-start_time)
     plt.scatter(x, transmission)
     plt.show()
 
